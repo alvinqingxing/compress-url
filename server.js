68343ab77aac8e9bbf366cbfcae6ae5a97f0b510
Alvin Lim <alvinch@hawaii.edu>
2020-08-02 21:40:17 +0800

    Server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
 const ShortUrl = require('./models/shortUrl')
 
 mongoose.connect(process.env.DB_URI, {
